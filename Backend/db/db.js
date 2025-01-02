0c0a39a6058cc83c0894bec84ac55398bbd438f6
SHADABALIGITHUB <shad46243@gmail.com>
2025-01-02 18:30:09 +0530

    Todo list Started for internship frontend

--- Backend/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/SHADAB");
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error:"));
-
-
-module.exports = db;
