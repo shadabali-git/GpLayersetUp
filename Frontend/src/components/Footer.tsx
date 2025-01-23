0d4e653eb379c162890610fb47128055c146ce7b
shaddy <137039248+shadabali-git@users.noreply.github.com>
2025-01-24 01:50:30 +0530

    Delete Frontend directory

--- Frontend/src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer=()=>{
-    return (
-        <footer className="bg-gray-800 text-white p-4 text-center">
-            <p className="text-sm">© 2025 My App. All rights reserved.</p>
-        </footer>
-    )
-}
-export default Footer;
